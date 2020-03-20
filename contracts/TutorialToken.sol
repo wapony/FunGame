adee8b69c5697e157678328a3381d060c3b930be
rj-mayong <wapony@126.com>
2020-03-20 17:48:02 +0800

    编写js

--- contracts/TutorialToken.sol
+++ contracts/TutorialToken.sol
@@ -1,16 +1,24 @@
 pragma solidity >=0.4.24 < 0.7.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
 contract TutorialToken  {
+    using SafeMath for uint256;
+
     // string public name = "TutorialToken";
     // string public symbol = "TT";
     // uint8 public decimals = 2;
     // uint public INITIAL_SUPPLY = 12000;
     // address contractOwner;
 
+    // 记录一个地址总投资额度
+    mapping(address => uint) public ownerToAmount;
+
     constructor() public {
         // _mint(msg.sender, INITIAL_SUPPLY);
         // contractOwner = msg.sender;
+
+        ownerToAmount[msg.sender] = 0;
     }
 
     // 实现购买TT操作
@@ -23,7 +31,12 @@ contract TutorialToken  {
     // }
 
     // 转移以太币试试看能不能测试
-    function sellEth() payable public {
+    function sellEth(uint amount) public payable {
+        ownerToAmount[msg.sender] = ownerToAmount[msg.sender].add(amount);
+    }
 
+    // 获取用户的总投资额度
+    function getTotalAmount() public view returns(uint) {
+        return ownerToAmount[msg.sender];
     }
 }
