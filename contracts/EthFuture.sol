c78c3ac30f308f9edde699e4a4dc88977ba80623
rj-mayong <wapony@126.com>
2020-03-24 16:38:24 +0800

    开发购买门票,投注等功能

--- contracts/EthFuture.sol
+++ contracts/EthFuture.sol
@@ -9,16 +9,15 @@ contract EthFuture is TutorialToken, Ownable {
     uint8 level3Rate;
     uint8 level4Rate;
 
-    // 总共投资ETH的数量
-    mapping (address => uint) ownerToEthAmount;
+    event NewInvestment(uint investmentId,  uint investAmount, uint investmentTime);
 
     struct InvestInfo {
         uint256 investAmount;   // 投资额度
-        uint32  investTime;     // 当前投注时间戳
+        uint256 investTime;     // 当前投注时间戳
     }
 
     // 记录每笔投资的信息
-    InvestInfo[] investInfos;
+    InvestInfo[] public investInfos;
 
     // 限制释放比例在一个区间，防止输错, 1 ~ 20
     modifier rateRestrict(uint rate) {
@@ -26,9 +25,16 @@ contract EthFuture is TutorialToken, Ownable {
         _;
     }
 
+    mapping (uint => address) public investmentToOwner;
+    mapping (address => uint) public ownerInvestmentCount;
+
     // 创建投资信息
     function _createInvestInfo(uint _investAmount) private {
-        investInfos.push(InvestInfo(_investAmount, now));
+        uint id = investInfos.push(InvestInfo(_investAmount, now)) - 1;
+        investmentToOwner[id] = msg.sender;
+        ownerInvestmentCount[msg.sender] = ownerInvestmentCount[msg.sender].add(1);
+
+        emit NewInvestment(id, _investAmount, now);
     }
 
     function setLevel1Rate(uint8 _level1Rate) public onlyOwner rateRestrict(_level1Rate) {
@@ -60,34 +66,36 @@ contract EthFuture is TutorialToken, Ownable {
 
     // 投注
     function investGame() public payable returns(bool) {
-        // 调用者的TToken余额为
-
         // 限定最低1个eth起投,低于1个返回错误提示
         require(msg.value >= (10 ** 18));
 
-        ownerToEthAmount[msg.sender] = ownerToEthAmount[msg.sender].add(msg.value);
+        // 调用者的TToken余额为
+        uint ttokenBalance = balanceOf(msg.sender);
+        // 1eth = 2000Token, 门票为投注eth的 十分之一
+        uint needTtoken = (msg.value / (10 ** 18) ) * (2000 / 10);
 
-        // 创建每次投资的信息
-        uint ethMount = msg.value / (10 ** 18);
-        uint release;
-        if (ethMount >= 1 && ethMount < 6) {
-            release = msg.value * level1Rate / 1000;
-        } else if (ethMount >= 6 && ethMount < 11) {
-            release = msg.value * level2Rate / 1000;
-        } else if (ethMount >= 11 && ethMount < 30) {
-            release = msg.value * level3Rate / 1000;
-        } else {
-            release = msg.value * level4Rate / 1000;
-        }
+        // 要求账户ttoken的余额足够门票的支付
+        require(ttokenBalance >= needTtoken);
 
-        _createInvestInfo(msg.value, release);
+        // // 创建每次投资的信息
+        // uint ethMount = msg.value / (10 ** 18);
+        // uint release;
+        // if (ethMount >= 1 && ethMount < 6) {
+        //     release = msg.value * level1Rate / 1000;
+        // } else if (ethMount >= 6 && ethMount < 11) {
+        //     release = msg.value * level2Rate / 1000;
+        // } else if (ethMount >= 11 && ethMount < 30) {
+        //     release = msg.value * level3Rate / 1000;
+        // } else {
+        //     release = msg.value * level4Rate / 1000;
+        // }
+
+        _createInvestInfo(msg.value);
+
+        // 扣除账户的门票
+        _transfer(msg.sender, contractOwner, needTtoken);
 
         return true;
-    }
-
-    // 获取总投资额度
-    function getInvestTotalAmount() public view returns(uint) {
-        return ownerToEthAmount[msg.sender];
     }
 
     // 获取合约的剩余额度
@@ -96,14 +104,15 @@ contract EthFuture is TutorialToken, Ownable {
     }
 
     // 获取每日的释放额度, 问题有点复杂，需要再想想, 例如：第一次释放的已经释放完，总不能继续释放吧。
-    function getReleaseEthPerDay() public view returns(uint) {
-        // uint releaseTotalPerDay;
+    function getReleaseEthPerDay() internal returns(uint) {
+        
+    }
 
-        // for(uint i=0; i<investInfos.length; i++) {
-        //     InvestInfo storage temp = investInfos[i];
-        //     releaseTotalPerDay = releaseTotalPerDay.add(temp.releasePerDay);
-        // }
+    // 合约拥有者提币
+    function ownerGetBenefit(uint amount) external onlyOwner {
+        require((amount * (10 ** 18)) <= address(this).balance);
 
-        // return releaseTotalPerDay;
+        // 参数的单位是wei， 所以转移1ETH，需要10 ** 18.  1ETH = 10 ** 18 Wei
+        msg.sender.transfer(amount * (10 ** 18));
     }
 }
