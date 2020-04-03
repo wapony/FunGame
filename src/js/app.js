d6d4cd63f8dac34694f7e259363c481c713941b8
mayong <wapony@126.com>
2020-04-03 16:05:09 +0800

    加入metamask审批

--- src/js/app.js
+++ src/js/app.js
@@ -7,15 +7,19 @@ App = {
   },
 
   initWeb3: function () {
-    // Initialize web3 and set the provider to the testRPC.
-    if (typeof web3 !== 'undefined') {
-      App.web3Provider = web3.currentProvider;
-      web3 = new Web3(web3.currentProvider);
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        window.ethereum.enable();
+      } catch (error) {
+        console.error("user denied account access");
+      }
+    } else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
     } else {
-      // set the provider you want from Web3.providers
       App.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545');
-      web3 = new Web3(App.web3Provider);
     }
+    web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
@@ -99,7 +103,7 @@ App = {
 };
 
 $(function () {
-  $(window).load(function () {
+  $(window).on("load", function () {
     App.init();
   });
 });
