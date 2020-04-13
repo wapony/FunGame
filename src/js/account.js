af24a069da4e84ae2ac6dea4b2708a531ba2a7d2
mayong <wapony@126.com>
2020-04-13 17:45:48 +0800

    测试页面

--- src/js/account.js
+++ src/js/account.js
@@ -1,6 +1,37 @@
-$(function(){  
-    var mydata={basenum:"123",basenum2:236};
-	$("#jishu").val(mydata.basenum);
-    $("#jishu1").val(mydata.basenum2);
+$(function(){ 
+	//封装查询数据todo
+    var mydata={basenum1:"123",basenum2:236,basenum3:236,basenum4:236,basenum5:236,basenum6:236,basenum7:236,basenum8:236,basenum9:236,basenum10:236,basenum11:236,basenum12:236,basenum13:236};
+	showData(mydata);
+	getoutETH();
 }); 
+
+function showData(mydata){
+	$("#id1").val(mydata.basenum1);
+    $("#id2").val(mydata.basenum2);
+	$("#id3").val(mydata.basenum3);
+    $("#id4").val(mydata.basenum4);
+	$("#id5").val(mydata.basenum5);
+    $("#id6").val(mydata.basenum6);
+	$("#id7").val(mydata.basenum7);
+    $("#id8").val(mydata.basenum8);
+	$("#id9").val(mydata.basenum9);
+    $("#id10").val(mydata.basenum10);
+	$("#id11").val(mydata.basenum11);
+    $("#id12").val(mydata.basenum12);
+	$("#id13").val(mydata.basenum13);
+    
+}
+function getoutETH(){
+	//数量
+	var num=$("#id13").val();
+	//密码
+	
+	//按钮点击事件
+	$("#getethButton").click(function(){
+		//调用接口todo
+		var pwd =$("#pwdId").val();
+		alert(pwd);
+	});
+	
+}
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,18 @@
+
+
+
 $(function(){  
-	//$('.numclass').click(function(){
-	$(".numclass").each(function(){
-		$(this).click(function(){
-			//console.error("11111:"+$(this).attr("value"));
-			$('#enterNumId').val($(this).attr('value'));
-		});
+	// $(".numclass").each(function(){
+	// 	$(this).click(function(){
+	// 		//console.error("11111:"+$(this).attr("value"));
+	// 		$('#enterNumId').val($(this).attr('value'));
+	// 	});
 		
-	});
+	// });
+
+	var app = getApp();
+	console.log("sssss " + app);
+
     
 }); 
 
